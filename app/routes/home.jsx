99e45f83cd9ed012773bb4a97472a0664ed68427
gabrieleminafra <gabriele.mina@icloud.com>
2025-02-27 16:18:12 +0100

    incremenal commit

--- app/routes/home.jsx
+++ app/routes/home.jsx
@@ -6,6 +6,7 @@ import {
   ArchiveIcon,
   CloudUpload,
   Download,
+  Fullscreen,
   PlusIcon,
   Save,
   Trash,
@@ -33,7 +34,7 @@ const UploadedFile = ({
   return (
     <li className="w-full bg-white rounded-xl" key={archive.id}>
       {/* File Input */}
-      <label className="w-full p-6 gap-4 flex justify-between items-center border-2 border-gray-300 rounded-lg">
+      <label className="w-full p-6 gap-4 flex justify-between items-center border-2 border-gray-300 rounded-xl">
         <div className="text-gray-600 whitespace-nowrap w-10/12 overflow-hidden">
           <span className="font-semibold">{archive.title}</span>
           <br />
@@ -76,10 +77,12 @@ export default function Home() {
   const [archiveItems, setArchiveItems] = useState([]);
   const [pendingChanges, setPendingChanges] = useState(0);
 
-  const [isFocused, setIsFocused] = useState(true);
   const [file, setFile] = useState(null);
+  const [isFocused, setIsFocused] = useState(true);
   const [isUploading, setIsUploading] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+  const [isFullScreen, setFullScreen] = useState(false);
+  const [isInitialLoadComplete, setIsInitialLoadComplete] = useState(false);
 
   const handlePastePublish = async (body) => {
     try {
@@ -119,6 +122,7 @@ export default function Home() {
     } catch (error) {
       console.error(error);
     }
+    if (!isInitialLoadComplete) setIsInitialLoadComplete(true);
     setIsLoading(false);
   };
 
@@ -277,180 +281,205 @@ export default function Home() {
   return (
     <Fragment>
       <Toaster />
-      {/* {isLoading && ( */}
       <div
         className={`${
           isLoading ? "opacity-100" : "opacity-0 pointer-events-none"
-        } fixed inset-0 flex items-center justify-center transition-opacity duration-500 bg-white/50`}
+        } fixed inset-0 flex items-center justify-center transition-opacity duration-500 bg-white/70 z-50`}
       >
-        <div className="flex flex-col items-center p-6 bg-white rounded-2xl shadow-lg opacity-100">
+        <div className="flex flex-col items-center opacity-100">
           <div className="animate-spin h-10 w-10 border-4 border-blue-500 border-t-transparent rounded-full mb-4"></div>
           <p className="text-lg font-semibold text-gray-700">Sincronizzo...</p>
         </div>
       </div>
       {/* )} */}
 
-      <div className="flex flex-col xl:flex-row h-lvh bg-gray-100 p-4 py-12 gap-8 xl:gap-4">
-        <div className="w-full xl:min-w-3/12 flex-1 bg-white p-6 rounded-2xl shadow-xl">
-          <div className="flex flex-col justify-between h-full gap-4">
-            <h2 className="text-xl font-semibold text-gray-800">Appunti</h2>
-            {!isLoading && pasteItems.length == 0 && (
-              <div className="h-full grid place-content-center">
-                <img
-                  className="aspect-square h-56 grayscale opacity-70"
-                  src={empty_1}
-                />
-              </div>
-            )}
-            <ul className="space-y-3 overflow-y-auto grow">
-              {pasteItems.map((item) => (
-                <li
-                  key={item.id}
-                  onClick={() => setSelectedPaste(item)}
-                  className={`p-3 hover:bg-gray-400 rounded-lg text-gray-700 font-semibold cursor-pointer transition ${
-                    selectedPaste?.id === item.id ? "bg-sky-300" : "bg-gray-200"
-                  }`}
-                >
-                  {item.title.slice(0, 30)}
-                </li>
-              ))}
-            </ul>
-            <div className="space-y-3 ">
-              <button
-                onClick={() => setSelectedPaste(INITIAL_PASTE_DATA)}
-                className="p-3 w-full bg-sky-600 hover:bg-sky-800 rounded-lg text-white font-semibold transition flex justify-center cursor-pointer gap-4"
-              >
-                Crea nuovo blocco <PlusIcon />
-              </button>
-            </div>
-          </div>
-        </div>
-
-        {selectedPaste && (
-          <div className="w-full xl:w-5/12 flex flex-col items-center justify-center">
-            <div className="w-full bg-white p-6 rounded-xl shadow-xl flex flex-col gap-4 flex-1">
-              <div className="flex justify-between items-center">
-                <h2 className="text-xl font-semibold text-gray-800">
-                  {selectedPaste.created_at
-                    ? `Paste del ${new Date(
-                        selectedPaste.created_at
-                      ).toLocaleString("it-IT")}`
-                    : "Nuova bozza appunti"}
-                </h2>
-                <X onClick={() => setSelectedPaste(null)} />
-              </div>
-              <input
-                type="text"
-                value={selectedPaste.title}
-                onChange={(e) =>
-                  setSelectedPaste((state) => ({
-                    ...state,
-                    title: e.target.value,
-                  }))
-                }
-                onFocus={handleFocus}
-                onBlur={handleBlur}
-                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-                placeholder="Inserisci un titolo"
-              />
-              <textarea
-                value={selectedPaste.content}
-                onChange={(e) =>
-                  setSelectedPaste((state) => ({
-                    ...state,
-                    content: e.target.value,
-                  }))
-                }
-                onFocus={handleFocus}
-                onBlur={handleBlur}
-                className="w-full p-3 border border-gray-300 rounded-lg h-full focus:outline-none focus:ring-2 focus:ring-blue-400"
-                placeholder="Scrivi qui..."
-                rows={20}
-              ></textarea>
-
-              {!selectedPaste.id ? (
-                <button
-                  className="p-3 bg-sky-600 hover:bg-sky-800 rounded-lg font-semibold text-white cursor-pointer transition flex justify-center gap-4"
-                  onClick={() =>
-                    handlePastePublish({
-                      title: selectedPaste.title,
-                      content: selectedPaste.content,
-                    })
-                  }
-                >
-                  Salva appunti
-                  <Save />
-                </button>
-              ) : (
-                <button
-                  className="p-3 bg-red-400 text-white rounded-lg font-semibold cursor-pointer transition flex justify-center"
-                  onClick={() => handlePasteDelete(selectedPaste.id)}
-                >
-                  Archivia questi appunti
-                  <ArchiveIcon className="ml-3" />
-                </button>
-              )}
-            </div>
-          </div>
-        )}
-
-        <div className="w-full xl:min-w-4/12 flex-1 bg-white p-6 rounded-2xl shadow-xl">
-          <div className="flex flex-col justify-between h-full gap-4">
-            <h2 className="text-xl font-semibold text-gray-800">Documenti</h2>
-
-            <ul className="overflow-y-auto grow space-y-3 ">
-              {!isLoading && archiveItems.length == 0 && (
-                <div className="h-full grid place-content-center">
-                  <img
-                    className="aspect-square h-56 grayscale opacity-70"
-                    src={empty_2}
-                  />
-                </div>
-              )}
-              {archiveItems.map((archive) => (
-                <UploadedFile
-                  key={archive.id}
-                  archive={archive}
-                  handleArchiveDelete={handleArchiveDelete}
-                  handleArchiveDownload={handleArchiveDownload}
-                />
-              ))}
-            </ul>
-
-            <div className="w-full bg-white rounded-xl flex flex-col">
-              {/* File Input */}
-              <label
-                className={`w-full flex flex-col items-center justify-center border-2 border-dashed border-gray-300 rounded-lg ${
-                  isUploading
-                    ? "animate-pulse"
-                    : "cursor-pointer hover:border-blue-400"
-                }`}
-              >
-                <div className="text-gray-600">
-                  {isUploading ? (
-                    <div className="flex items-center justify-between gap-4 ">
-                      <div className="flex items-center justify-center py-4">
-                        <div className="w-8 h-8 border-4 border-blue-300 border-t-blue-900 rounded-full animate-spin"></div>
-                      </div>
-                      <span>Carico {file?.name.slice(0, 35)}...</span>
-                    </div>
-                  ) : (
-                    <div className="flex items-center justify-center gap-4 my-2">
-                      <span>Carica un documento</span>
-                      <CloudUpload className="w-7 h-7  text-blue-500" />
+      <div className="bg-fixed inset-0 min-h-screen bg-gray-100">
+        {isInitialLoadComplete && (
+          <div className="flex flex-col xl:flex-row h-max xl:h-screen gap-8 xl:gap-4 p-4 py-12">
+            {!isFullScreen && (
+              <div className="w-full xl:min-w-3/12 flex-1 bg-white p-6 rounded-xl shadow-xl">
+                <div className="flex flex-col justify-between h-full gap-4">
+                  <h2 className="text-xl font-semibold text-gray-800">
+                    Appunti
+                  </h2>
+                  {pasteItems.length == 0 && (
+                    <div className="h-full grid place-content-center">
+                      <img
+                        className="aspect-square h-56 grayscale opacity-70"
+                        src={empty_1}
+                      />
                     </div>
                   )}
+                  <ul className="space-y-3 overflow-y-auto grow">
+                    {pasteItems.map((item) => (
+                      <li
+                        key={item.id}
+                        onClick={() => setSelectedPaste(item)}
+                        className={`p-3 hover:bg-gray-400 rounded-xl text-gray-700 font-semibold cursor-pointer transition ${
+                          selectedPaste?.id === item.id
+                            ? "bg-sky-300"
+                            : "bg-gray-200"
+                        }`}
+                      >
+                        {item.title.slice(0, 30)}
+                      </li>
+                    ))}
+                  </ul>
+                  <div className="space-y-3 ">
+                    <button
+                      onClick={() => setSelectedPaste(INITIAL_PASTE_DATA)}
+                      className="p-3 w-full bg-sky-600 hover:bg-sky-800 rounded-xl text-white font-semibold transition flex justify-center cursor-pointer gap-4"
+                    >
+                      Crea nuovo blocco <PlusIcon />
+                    </button>
+                  </div>
                 </div>
-                <input
-                  disabled={isUploading}
-                  type="file"
-                  className="hidden"
-                  onChange={handleFileChange}
-                />
-              </label>
-            </div>
+              </div>
+            )}
+
+            {selectedPaste && (
+              <div className="w-full xl:min-w-4/12 flex flex-col items-center justify-center h-full">
+                <div className="w-full bg-white p-6 rounded-xl shadow-xl flex flex-col gap-4 flex-1">
+                  <div className="flex justify-between items-center">
+                    <h2 className="text-xl font-semibold text-gray-800">
+                      {selectedPaste.created_at
+                        ? `Appunti del ${new Date(
+                            selectedPaste.created_at
+                          ).toLocaleString("it-IT")}`
+                        : "Nuova bozza appunti"}
+                    </h2>
+                    <div className="flex gap-2">
+                      <Fullscreen
+                        className="cursor-pointer hover:bg-gray-200 rounded-xl p-1"
+                        onClick={() => setFullScreen(!isFullScreen)}
+                      />
+                      {!isFullScreen && (
+                        <X
+                          className="cursor-pointer hover:bg-gray-200 rounded-xl p-1"
+                          onClick={() => setSelectedPaste(null)}
+                        />
+                      )}
+                    </div>
+                  </div>
+
+                  <input
+                    type="text"
+                    value={selectedPaste.title}
+                    onChange={(e) =>
+                      setSelectedPaste((state) => ({
+                        ...state,
+                        title: e.target.value,
+                      }))
+                    }
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
+                    className="w-full p-3 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400"
+                    placeholder="Inserisci un titolo"
+                  />
+                  <textarea
+                    value={selectedPaste.content}
+                    onChange={(e) =>
+                      setSelectedPaste((state) => ({
+                        ...state,
+                        content: e.target.value,
+                      }))
+                    }
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
+                    className="w-full p-3 border border-gray-300 rounded-xl h-full focus:outline-none focus:ring-2 focus:ring-blue-400"
+                    placeholder="Scrivi qui..."
+                    rows={18}
+                  ></textarea>
+
+                  {!selectedPaste.id ? (
+                    <button
+                      className="p-3 bg-sky-600 hover:bg-sky-800 rounded-xl font-semibold text-white cursor-pointer transition flex justify-center gap-4"
+                      onClick={() =>
+                        handlePastePublish({
+                          title: selectedPaste.title,
+                          content: selectedPaste.content,
+                        })
+                      }
+                    >
+                      Salva appunti
+                      <Save />
+                    </button>
+                  ) : (
+                    <button
+                      className="p-3 bg-red-400 text-white rounded-xl font-semibold cursor-pointer transition flex justify-center"
+                      onClick={() => handlePasteDelete(selectedPaste.id)}
+                    >
+                      Archivia questi appunti
+                      <ArchiveIcon className="ml-3" />
+                    </button>
+                  )}
+                </div>
+              </div>
+            )}
+
+            {!isFullScreen && (
+              <div className="w-full xl:min-w-4/12 flex-1 bg-white p-6 rounded-xl shadow-xl">
+                <div className="flex flex-col justify-between h-full gap-4">
+                  <h2 className="text-xl font-semibold text-gray-800">
+                    Documenti
+                  </h2>
+
+                  <ul className="overflow-y-auto grow space-y-3 ">
+                    {archiveItems.length == 0 && (
+                      <div className="h-full grid place-content-center">
+                        <img
+                          className="aspect-square h-56 grayscale opacity-70"
+                          src={empty_2}
+                        />
+                      </div>
+                    )}
+                    {archiveItems.map((archive) => (
+                      <UploadedFile
+                        key={archive.id}
+                        archive={archive}
+                        handleArchiveDelete={handleArchiveDelete}
+                        handleArchiveDownload={handleArchiveDownload}
+                      />
+                    ))}
+                  </ul>
+
+                  <div className="w-full bg-white rounded-xl flex flex-col">
+                    {/* File Input */}
+                    <label
+                      className={`w-full flex flex-col items-center justify-center border-2 border-dashed border-gray-300 rounded-xl ${
+                        isUploading
+                          ? "animate-pulse"
+                          : "cursor-pointer hover:border-blue-400"
+                      }`}
+                    >
+                      <div className="text-gray-600">
+                        {isUploading ? (
+                          <div className="flex items-center justify-between gap-4 ">
+                            <div className="flex items-center justify-center py-4">
+                              <div className="w-8 h-8 border-4 border-blue-300 border-t-blue-900 rounded-full animate-spin"></div>
+                            </div>
+                            <span>Carico {file?.name.slice(0, 35)}...</span>
+                          </div>
+                        ) : (
+                          <div className="flex items-center justify-center gap-4 my-2">
+                            <span>Carica un documento</span>
+                            <CloudUpload className="w-7 h-7  text-blue-500" />
+                          </div>
+                        )}
+                      </div>
+                      <input
+                        disabled={isUploading}
+                        type="file"
+                        className="hidden"
+                        onChange={handleFileChange}
+                      />
+                    </label>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
-        </div>
+        )}
       </div>
     </Fragment>
   );
